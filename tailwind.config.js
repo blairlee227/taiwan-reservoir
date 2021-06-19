4b3e81a801952b54e7825416ab485d64057c2ca7
blairlee227 <blairlee227@gmail.com>
2021-06-19 17:32:04 +0800

    create tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// const colors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  theme: {
+    extend: {
+          colors: {
+            brown: '#806e49',
+            grass: '#86b71b',
+            'beige': '#f8f6f3',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
