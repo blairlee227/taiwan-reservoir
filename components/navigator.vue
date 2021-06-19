3b6c0f7acb285c01f2f9b64bd045bf5cfba80fbc
blairlee227 <blairlee227@gmail.com>
2021-06-19 17:31:35 +0800

    create. nav

---
+++ components/navigator.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="bg-brown py-1"></div>
+    <div class="container mx-auto pt-10 px-20">
+      <div class="grid grid-cols-6 gap-1">
+        <div class="col-span-1">
+          <img src="../assets/images/logo.svg" />
+        </div>
+        <!-- <div class="col-span-1"></div> -->
+        <!-- <div class="col-span-4">
+          資料來源：經濟部水利署
+          <br />即時水情資料係自記儀器自動產生，未經人工完整檢驗，僅提供參考。
+          <br />根據水利署網頁公布，各項水庫資料由各水庫管理單位在每日輸入，更新時間不一致。（部分水庫星期六、日之資料則在星期一統一輸入）
+        </div> -->
+      </div>
+    </div>
+  </div>
+</template>
