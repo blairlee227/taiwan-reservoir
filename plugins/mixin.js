8c64fcdccbf984f52f904d2216c2cc79c9795590
blairlee227 <blairlee227@gmail.com>
2021-06-19 18:36:30 +0800

    update. mixin
    將 method 搬至頁面上

--- plugins/mixin.js
+++ plugins/mixin.js
@@ -1,97 +1,116 @@
 import Vue from "vue";
 
 let mixin = {
-  methods: {
-    findArea(name) {
-      let areaData = [
+  data() {
+    return {
+      areaData:[
         {
           name: "新山水庫",
-          area: ["基隆"]
+          area: ["基隆"],
+          mainArea: "北部"
         },
         {
           name: "翡翠水庫",
-          area: ["台北", "新北"]
+          area: ["台北", "新北"],
+          mainArea: "北部"
         },
         {
           name: "石門水庫",
-          area: ["新北", "桃園", "新竹"]
+          area: ["新北", "桃園", "新竹"],
+          mainArea: "北部"
         },
         {
           name: "永和山水庫",
-          area: ["新竹", "苗栗"]
+          area: ["新竹", "苗栗"],
+          mainArea: "北部"
         },
         {
           name: "寶山水庫",
-          area: ["新竹"]
+          area: ["新竹"],
+          mainArea: "北部"
         },
         {
           name: "寶山第二水庫",
-          area: ["新竹"]
+          area: ["新竹"],
+          mainArea: "北部"
         },
         {
           name: "明德水庫",
-          area: ["苗栗"]
+          area: ["苗栗"],
+          mainArea: "北部"
         },
         {
           name: "鯉魚潭水庫",
-          area: ["苗栗", "台中"]
+          area: ["苗栗", "台中"],
+          mainArea: "中部"
         },
         {
           name: "德基水庫",
-          area: ["台中"]
+          area: ["台中"],
+          mainArea: "中部"
         },
         {
           name: "石岡壩",
-          area: ["台中"]
+          area: ["台中"],
+          mainArea: "中部"
         },
         {
           name: "日月潭水庫",
-          area: ["南投"]
+          area: ["南投"],
+          mainArea: "中部"
         },
         {
           name: "霧社水庫",
-          area: ["南投"]
+          area: ["南投"],
+          mainArea: "中部"
         },
         {
           name: "湖山水庫",
-          area: ["雲林", "彰化", "嘉義"]
+          area: ["雲林", "彰化", "嘉義"],
+          mainArea: "中部"
         },
         {
           name: "仁義潭水庫",
-          area: ["嘉義"]
+          area: ["嘉義"],
+          mainArea: "南部"
         },
         {
           name: "蘭潭水庫",
-          area: ["嘉義"]
+          area: ["嘉義"],
+          mainArea: "南部"
         },
         {
           name: "白河水庫",
-          area: ["台南"]
+          area: ["台南"],
+          mainArea: "南部"
         },
         {
           name: "曾文水庫",
-          area: ["嘉義", "台南"]
+          area: ["嘉義", "台南"],
+          mainArea: "南部"
         },
         {
           name: "烏山頭水庫",
-          area: ["台南"]
+          area: ["台南"],
+          mainArea: "南部"
         },
         {
           name: "南化水庫",
-          area: ["台南", "高雄"]
+          area: ["台南", "高雄"],
+          mainArea: "南部"
         },
         {
           name: "阿公店水庫",
-          area: ["高雄"]
+          area: ["高雄"],
+          mainArea: "南部"
         },
         {
           name: "牡丹水庫",
-          area: ["屏東"]
-        },
+          area: ["屏東"],
+          mainArea: "南部"
+        }
       ]
-      let area = areaData.find(item => name === item.name);
-      return area.area;
-    },
+    }
   }
 };
 Vue.mixin(mixin);
