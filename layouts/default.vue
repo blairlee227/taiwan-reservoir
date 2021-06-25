ef6cf85ca8bb65c495c5903a752e7bc8e6d86792
blairlee227 <blairlee227@gmail.com>
2021-06-25 15:36:52 +0800

    update. layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <navigator />
     <div
-      class="container mx-auto pt-10 xl:px-20 lg:px-10 md:px-10 sm:px-10 px-5"
+      class="container mx-auto pt-5 xl:px-20 lg:px-10 md:px-10 sm:px-10 px-5"
     >
       <div class="bg-beige rounded-3xl mb-10">
         <Nuxt />
