5a1eaf5f13bbb1b9cbe0f70e96a79f08d9959c57
blairlee227 <blairlee227@gmail.com>
2021-06-26 20:37:49 +0800

    remove .nuxt files

--- .nuxt/components/index.js
+++
@@ -1,9 +0,0 @@
-import { wrapFunctional } from './utils'
-
-export { default as Chart } from '../../components/chart.vue'
-export { default as Navigator } from '../../components/navigator.vue'
-export { default as ReservoirInfo } from '../../components/reservoirInfo.vue'
-
-export const LazyChart = import('../../components/chart.vue' /* webpackChunkName: "components/chart" */).then(c => wrapFunctional(c.default || c))
-export const LazyNavigator = import('../../components/navigator.vue' /* webpackChunkName: "components/navigator" */).then(c => wrapFunctional(c.default || c))
-export const LazyReservoirInfo = import('../../components/reservoirInfo.vue' /* webpackChunkName: "components/reservoir-info" */).then(c => wrapFunctional(c.default || c))
