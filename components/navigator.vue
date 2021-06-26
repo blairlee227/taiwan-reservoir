ad2e5d529476b323388ccefe0dbd560441e8d771
blairlee227 <blairlee227@gmail.com>
2021-06-26 20:34:58 +0800

    update. ui

--- components/navigator.vue
+++ components/navigator.vue
@@ -5,23 +5,6 @@
       class="container mx-auto pt-10 xl:px-20 lg:px-10 md:px-10 sm:px-10 px-5"
     >
       <img class="w-64 mx-auto" src="../assets/images/logo.svg" />
-      <div
-        class="
-          p-5
-          mt-10
-          border border-solid border-brown border-opacity-40
-          rounded-3xl
-        "
-      >
-        <p class="text-brown leading-7">
-          <span class="material-icons-outlined"> done </span>
-          資料來源：經濟部水利署
-          <br />
-          即時水情資料係自記儀器自動產生，未經人工完整檢驗，僅提供參考。
-          <br />
-          根據水利署網頁公布，各項水庫資料由各水庫管理單位在每日輸入，更新時間不一致。（部分水庫星期六、日之資料則在星期一統一輸入）
-        </p>
-      </div>
     </div>
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,10 +4,11 @@
     <div
       class="container mx-auto pt-5 xl:px-20 lg:px-10 md:px-10 sm:px-10 px-5"
     >
-      <div class="bg-beige rounded-3xl mb-10">
+      <div class="bg-beige rounded-3xl">
         <Nuxt />
       </div>
     </div>
+    <footerText />
   </div>
 </template>
 <style lang="scss">
