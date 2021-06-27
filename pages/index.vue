5271ecf770f55078bc00c29a9d91c8cebaac2fd6
blairlee227 <blairlee227@gmail.com>
2021-06-27 21:06:27 +0800

    update. add filter

--- pages/index.vue
+++ pages/index.vue
@@ -10,15 +10,20 @@
           py-1
           px-2
           align-middle
+          cursor-pointer
+          duration-300
+          ease-in-out
         "
+        :class="{ 'bg-opacity-90': areaBtn.active }"
         v-for="areaBtn in areaBtns"
-        :key="areaBtn.name"
+        :key="areaBtn.value"
+        @click="dataFilter(areaBtn)"
       >
-        {{ areaBtn.name }}
+        {{ areaBtn.text }}
       </span>
     </div>
     <div class="grid lg:grid-cols-2 sm:grid-cols-1 gap-4 mt-12">
-      <div class="mb-10" v-for="item in rowData" :key="item.name">
+      <div class="mb-10" v-for="item in dataForView" :key="item.name">
         <reservoirInfo :rowData="item" />
       </div>
     </div>
@@ -30,18 +35,27 @@ export default {
   data() {
     return {
       rowData: [],
+      dataForView: [],
       areaBtns: [
         {
-          name: "全部"
+          text: "全部",
+          value: "ALL",
+          active: true
         },
         {
-          name: "北部"
+          text: "北部",
+          value: "NORTHERN",
+          active: false
         },
         {
-          name: "中部"
+          text: "中部",
+          value: "CENTRAL",
+          active: false
         },
         {
-          name: "南部"
+          text: "南部",
+          value: "SOUTHERN",
+          active: false
         }
       ]
     };
@@ -57,10 +71,10 @@ export default {
         item.sort = this.findData(item.name, "sort");
         return item;
       });
-      console.log(this.rowData);
       this.rowData.sort(function (a, b) {
         return a.sort - b.sort;
       });
+      this.dataForView = this.rowData;
     } catch (e) {
       alert(e);
     }
@@ -69,6 +83,20 @@ export default {
     findData(name, key) {
       let area = this.areaData.find((item) => name === item.name);
       return area[key];
+    },
+    dataFilter(item) {
+      // clear all btn style
+      this.areaBtns.forEach((btn) => (btn.active = false));
+      // add active btn style
+      item.active = true;
+      // filter data
+      if (item.value === "ALL") {
+        this.dataForView = this.rowData;
+      } else {
+        this.dataForView = this.rowData.filter(
+          (data) => data.mainArea === item.value
+        );
+      }
     }
   }
 };
--- plugins/mixin.js
+++ plugins/mixin.js
@@ -7,127 +7,127 @@ let mixin = {
         {
           name: "新山水庫",
           area: ["基隆"],
-          mainArea: "北部",
+          mainArea: "NORTHERN",
           sort:1
         },
         {
           name: "翡翠水庫",
           area: ["台北", "新北"],
-          mainArea: "北部",
+          mainArea: "NORTHERN",
           sort:2
         },
         {
           name: "石門水庫",
           area: ["新北", "桃園", "新竹"],
-          mainArea: "北部",
+          mainArea: "NORTHERN",
           sort:3
         },
         {
           name: "永和山水庫",
           area: ["新竹", "苗栗"],
-          mainArea: "北部",
+          mainArea: "NORTHERN",
           sort:6
         },
         {
           name: "寶山水庫",
           area: ["新竹"],
-          mainArea: "北部",
+          mainArea: "NORTHERN",
           sort:4
         },
         {
           name: "寶山第二水庫",
           area: ["新竹"],
-          mainArea: "北部",
+          mainArea: "NORTHERN",
           sort:5
         },
         {
           name: "明德水庫",
           area: ["苗栗"],
-          mainArea: "北部",
+          mainArea: "NORTHERN",
           sort:7
         },
         {
           name: "鯉魚潭水庫",
           area: ["苗栗", "台中"],
-          mainArea: "中部",
+          mainArea: "CENTRAL",
           sort:8
         },
         {
           name: "德基水庫",
           area: ["台中"],
-          mainArea: "中部",
+          mainArea: "CENTRAL",
           sort:9
         },
         {
           name: "石岡壩",
           area: ["台中"],
-          mainArea: "中部",
+          mainArea: "CENTRAL",
           sort:10
         },
         {
           name: "日月潭水庫",
           area: ["南投"],
-          mainArea: "中部",
+          mainArea: "CENTRAL",
           sort:11
         },
         {
           name: "霧社水庫",
           area: ["南投"],
-          mainArea: "中部",
+          mainArea: "CENTRAL",
           sort:12
         },
         {
           name: "湖山水庫",
           area: ["雲林", "彰化", "嘉義"],
-          mainArea: "中部",
+          mainArea: "CENTRAL",
           sort:13
         },
         {
           name: "仁義潭水庫",
           area: ["嘉義"],
-          mainArea: "南部",
+          mainArea: "SOUTHERN",
           sort:14
         },
         {
           name: "蘭潭水庫",
           area: ["嘉義"],
-          mainArea: "南部",
+          mainArea: "SOUTHERN",
           sort:15
         },
         {
           name: "白河水庫",
           area: ["台南"],
-          mainArea: "南部",
+          mainArea: "SOUTHERN",
           sort:17
         },
         {
           name: "曾文水庫",
           area: ["嘉義", "台南"],
-          mainArea: "南部",
+          mainArea: "SOUTHERN",
           sort:16
         },
         {
           name: "烏山頭水庫",
           area: ["台南"],
-          mainArea: "南部",
+          mainArea: "SOUTHERN",
           sort:18
         },
         {
           name: "南化水庫",
           area: ["台南", "高雄"],
-          mainArea: "南部",
+          mainArea: "SOUTHERN",
           sort:19
         },
         {
           name: "阿公店水庫",
           area: ["高雄"],
-          mainArea: "南部",
+          mainArea: "SOUTHERN",
           sort:20
         },
         {
           name: "牡丹水庫",
           area: ["屏東"],
-          mainArea: "南部",
+          mainArea: "SOUTHERN",
           sort:21
         }
       ]
