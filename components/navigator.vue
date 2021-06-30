d3fa14cbded481f8c5c5891246894b2d5f1dbdfa
blairlee227 <blairlee227@gmail.com>
2021-06-30 19:56:27 +0800

    update. nav

--- components/navigator.vue
+++ components/navigator.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
-    <div class="bg-brown py-1"></div>
+    <div class="bg-brown py-2"></div>
     <div
-      class="container mx-auto pt-10 xl:px-20 lg:px-10 md:px-10 sm:px-10 px-5"
+      class="
+        container
+        mx-auto
+        pt-10
+        pb-5
+        xl:px-20
+        lg:px-10
+        md:px-10
+        sm:px-10
+        px-5
+      "
     >
-      <img class="w-64 mx-auto" src="../assets/images/logo.svg" />
+      <img class="w-96 mx-auto" src="../assets/images/logo.svg" />
     </div>
   </div>
 </template>
