e610c7abb5690c853c8a30e6f1df651bfb57ebed
blairlee227 <blairlee227@gmail.com>
2021-07-01 16:01:36 +0800

    update. footer

--- components/footerText.vue
+++ components/footerText.vue
@@ -3,6 +3,14 @@
     <div class="bg-brown py-3 mt-10">
       <p class="text-white text-opacity-70 leading-7 text-center">
         資料來源：經濟部水利署
+        <br />
+        <router-link to="https://github.com/blairlee227/taiwan-reservoir">
+          <img
+            class="inline-block w-6 opacity-70 align-middle mr-1"
+            src="../assets/images/github_icon.svg"
+          />
+          <span class="align-middle underline">blairlee227</span>
+        </router-link>
       </p>
     </div>
   </div>
